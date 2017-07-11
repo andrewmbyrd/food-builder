0bd6aebb478103fb23d958ab9b8971a0642df1e9
Andrew Byrd <andrewmbyrd@gmail.com>
2017-07-10 20:14:40 -0500

    Added ingredients

--- src/index.js
+++ src/index.js
@@ -6,45 +6,58 @@ class Menu extends React.Component {
     super();
     this.state = {
       foods: ["Pizza", "Hamburger", "Hot Dog", "Ice Cream"],
-      showFoods: true,
-      menuHeading: "Hello! Please Select which type of food you would like"
+      selectedFood: null
     };
   }
   
   handleFoodClick(food){
     console.log(food);
     this.setState({
-      showFoods: !this.state.showFoods,
-      menuHeading: `Select what you would like on your ${food}`
-    });
+      selectedFood: food
+    })
+  }
+  
+  handleIngredientClick(ingredient){
+    console.log(ingredient);
   }
 
   
   render() {
     let foodList;
-    const clickAction = this.handleFoodClick.bind(this);
+    const selectFood = this.handleFoodClick.bind(this);
+    foodList = this.state.foods;
+
     
-    if( this.state.showFoods ) {
-      foodList = this.state.foods.map(function(food, index) {
-        return (<li className={`${food.split(' ').join('')}-option`}>
-                  <Food value={food} 
-                        key={index} 
-                        onClick={clickAction}
-                  />  
-                </li>);
-        
-      });
-    }
     
     return (
       <div className="main">
-        <h1>{this.state.menuHeading}</h1>
-        <ul className="food-list">
-          {foodList}
-        </ul>
+        <h1>Hello! Please Select which type of food you would like:</h1>
+        <FoodItems foods={foodList} onClick={selectFood}/>
+        <h2>What would you like on your {this.state.selectedFood}?</h2>
+        <FoodIngredients food={this.state.selectedFood}/>
       </div>  
     );
   }
+}
+
+class FoodItems extends React.Component{
+  renderFood(food){
+    return(
+      <Food value={food} onClick={this.props.onClick}/>
+    );
+  }
+
+  render() {
+    return (
+      <ul className="food-list">
+        {this.renderFood(this.props.foods[0])}
+        {this.renderFood(this.props.foods[1])}
+        {this.renderFood(this.props.foods[2])}
+        {this.renderFood(this.props.foods[3])}
+      </ul>
+    );
+  }
+
 }
 
 class Food extends React.Component {
@@ -58,6 +71,55 @@ class Food extends React.Component {
     );
   }
 }
+
+class FoodIngredients extends React.Component {
+  
+  renderIngredient(value){
+    return(
+      <Ingredient value={value} />
+    );
+  }
+  
+  render(){
+    let ingredients;
+
+    switch (this.props.food) {
+      case "Pizza":
+        ingredients = ["Pepperoni", "Sausage"];
+        break;
+      case "Hamburger":
+        ingredients = ["Ketchup", "Pickles"];
+        break;
+      case "Hot Dog":
+        ingredients = ["Relish", "Mustard"];
+        break;
+      case "Ice Cream":
+        ingredients = ["Chocolate Syrup", "Sprinkles"];
+        break;
+      default:
+        ingredients = ["dumb", "stupid"];
+    }
+    
+    return(
+      <ul className="ingredient-list">
+        <li>{this.renderIngredient(ingredients[0])}</li>
+        <li>{this.renderIngredient(ingredients[1])}</li>
+      </ul>
+    );
+  }
+}
+
+class Ingredient extends React.Component {
+  render(){
+      return(
+      <div className="ingredient">
+        <p>{this.props.value}</p>
+      </div>
+    );
+  }
+}
+
+
 
 
 ReactDOM.render(<Menu />, document.getElementById('root'));
